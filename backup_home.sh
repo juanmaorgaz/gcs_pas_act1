55f08e6cb0347a31b96e4c653ea2995caec83804
juanmaorgaz <juanma.orgaz@gmail.com>
2026-03-20 20:42:54 +0100

    check_services.sh

--- backup_home.sh
+++ backup_home.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Variables
-ORIGEN="/home/usuario"
+ORIGEN="/home/juanmanuel"
 DESTINO="/tmp"
-LOG_FILE="/tmp/backup_home.log"
+LOG_FILE="/var/log/backup_home.log"
 FECHA=$(date +%Y-%m-%d_%H-%M-%S)
 NOMBRE_ARCHIVO="backup_home_$FECHA.tar.gz"
   
 # 1. Crea el archivo comprimido [c: crear, z: comprimir (gzip), f: archivo, p: preservar permisos]
-echo "[INFO] Iniciando respaldo de $ORIGEN..." >> "$LOG_FILE"
-tar -czpf "$DESTINO/$NOMBRE_ARCHIVO" "$ORIGEN" >> "$LOG_FILE"
+echo "[INFO-$(date '+%Y-%m-%d %H:%M:%S')] Iniciando respaldo de $ORIGEN..." >> "$LOG_FILE"
+tar -czpf "$DESTINO/$NOMBRE_ARCHIVO" "$ORIGEN" >> "$LOG_FILE" 2>&1
 
 # 2. Verificación
 if [ $? -eq 0 ]; then
-    echo "[INFO] Respaldo creado con éxito en: $DESTINO/$NOMBRE_ARCHIVO" >> "$LOG_FILE"
+    echo "[INFO-$(date '+%Y-%m-%d %H:%M:%S')] Respaldo creado con éxito en: $DESTINO/$NOMBRE_ARCHIVO" >> "$LOG_FILE"
 else
-    echo "[ERROR] Hubo un error al crear el archivo de respaldo o el directorio $ORIGEN no existe." >> "$LOG_FILE"
+    echo "[ERROR-$(date '+%Y-%m-%d %H:%M:%S')] Hubo un error al crear el archivo de respaldo o el directorio $ORIGEN no existe." >> "$LOG_FILE"
 fi
---
+++ check_services.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+SERVICIOS=("nginx" "ssh")
+LOG_FILE="/var/log/servicios_report.log"
+
+# Verificación
+for servicio in "${SERVICIOS[@]}"; do
+    echo "[INFO-$(date '+%Y-%m-%d %H:%M:%S')] Verificando servicio $servicio..." >> "$LOG_FILE"
+    ESTADO=$(systemctl is-active "$servicio")
+    if [ "$ESTADO" == "active" ]; then
+        echo "[INFO-$(date '+%Y-%m-%d %H:%M:%S')] El servicio $servicio está activo." >> "$LOG_FILE"
+    else
+        echo "[ERROR-$(date '+%Y-%m-%d %H:%M:%S')] El servicio $servicio no está activo, intentando iniciarlo..." >> "$LOG_FILE"
+        systemctl start "$servicio" >> "$LOG_FILE" 2>&1
+        if [ $? -eq 0 ]; then
+            echo "[INFO-$(date '+%Y-%m-%d %H:%M:%S')] El servicio $servicio se ha iniciado correctamente." >> "$LOG_FILE"
+        else
+            echo "[ERROR-$(date '+%Y-%m-%d %H:%M:%S')] No se ha podido iniciar el servicio $servicio." >> "$LOG_FILE"
+        fi
+    fi
+done
